d6bf1c7cc287860ce58a7434a480481f9a4c01fd
BryanSalgadoCode <112642339+BryanSalgadoCode@users.noreply.github.com>
2025-02-13 12:31:40 -0500

    Initial Commit

---
+++ cpa-user-manager/src/main/java/com/cpausermanager/cpa_user_manager/CpaUserManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cpausermanager.cpa_user_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpaUserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpaUserManagerApplication.class, args);
+	}
+
+}
---
+++ cpa-user-manager/src/test/java/com/cpausermanager/cpa_user_manager/CpaUserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpausermanager.cpa_user_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpaUserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
